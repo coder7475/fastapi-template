87df47f6d57a37ff7a6c59457477acba8675bcd1
coder7475 <robiulhossain7475@gmail.com>
2025-08-03 19:10:22 +0600

    feat: add item retrieval route

--- app/main.py
+++ app/main.py
@@ -7,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 async def get_route():
     return {"message": "Hello, World!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {
+        "item_id": item_id
+    }
+
+
