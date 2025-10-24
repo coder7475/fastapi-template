fb6de751ac11d1aee6871c643707e7eb9eadc42a
coder7475 <robiulhossain7475@gmail.com>
2025-10-24 09:39:57 +0600

    feat: create models and schemas for database

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,8 @@
 # Database models (if using SQLAlchemy, etc.)
 from typing import List
 from app.schemas import Item
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
 
 # In-memory storage for items
 __all__ = ["items_db"]
@@ -12,4 +14,21 @@ items_db: List[Item] = [
 ]
 
 # SQLAlchemy Models
-# class Todo(Base):
+class Todo(Base):
+    '''
+    This code defines a Todo model for a SQLAlchemy ORM, representing a table in the database:
+
+    Table Name: __tablename__ = "todos" specifies the table name as todos.
+    Columns:
+        id: An integer primary key, with index=True for faster queries.
+        title: A string for the task title, indexed for quick searches.
+        description: A string to describe the task, also indexed.
+        completed: A Boolean indicating if the task is complete, defaulting to False.
+    '''
+    
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    completed = Column(Boolean, default=False)
--- app/schemas.py
+++ app/schemas.py
@@ -14,3 +14,20 @@ class ItemResponse(BaseModel):
 
 # This defines the public API of the module; only 'Item' will be imported with 'from app.schemas import *'
 __all__ = ["Item"]
+
+# The class defines a Todo model with title, description, completed status, and an ID for database
+# operations.
+# Validation for Todo Table
+class TodoBase(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
+class TodoCreate(TodoBase):
+    pass
+
+class Todo(TodoBase):
+    id: int
+
+    class Config:
+        orm_mode = True
